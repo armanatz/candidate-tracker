b06b7aafddd71fc93fdcb860141be48478233e08
Arman Attarzadeh <arman.atz@gmail.com>
2022-09-29 20:28:52 +0000

    Create useAxios hook

---
+++ src/hooks/useAxios.ts
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const API_PROTO = import.meta.env.VITE_API_PROTO;
+const API_DOMAIN = import.meta.env.VITE_API_DOMAIN;
+const API_ENDPOINT = import.meta.env.VITE_API_ENDPOINT;
+
+const BASE_URL = `${API_PROTO}://${API_DOMAIN}/${API_ENDPOINT}`;
+
+const axiosPublic = axios.create({
+  baseURL: BASE_URL,
+});
+
+export default function useAxio() {
+  return axiosPublic;
+}
