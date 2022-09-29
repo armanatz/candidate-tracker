1fc80c8d338fd0ca85c0d896615161ebb9af25c3
Arman Attarzadeh <arman.atz@gmail.com>
2022-09-29 17:57:18 +0000

    Remove unnecessary starter files

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,17 @@ import { useState } from 'react';
 
 import logo from './logo.svg';
 
-import styles from './App.module.scss';
-
 export default function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className={styles.container}>
-      <header className={styles.header}>
-        <img
-          src={logo}
-          className={styles.logo}
-          alt="logo"
-        />
+    <div>
+      <header>
+        <img src={logo} alt="logo" />
         <p>Hello Vite + React!</p>
         <p>
           <button
             type="button"
-            className={styles.btn}
             onClick={() =>
               setCount(prevState => prevState + 1)
             }
@@ -33,7 +26,6 @@ export default function App() {
         </p>
         <p>
           <a
-            className={styles.link}
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
@@ -42,7 +34,6 @@ export default function App() {
           </a>
           {' | '}
           <a
-            className={styles.link}
             href="https://vitejs.dev/guide/features.html"
             target="_blank"
             rel="noopener noreferrer"
