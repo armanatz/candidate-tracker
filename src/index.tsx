69cc0c4383e9cba8afc10cdb4baebd63a12670fd
Arman Attarzadeh <arman.atz@gmail.com>
2022-09-29 18:07:50 +0000

    Setup react-query

--- src/index.tsx
+++ src/index.tsx
@@ -5,20 +5,34 @@ import {
   Routes,
   Route,
 } from 'react-router-dom';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
 
 import './assets/styles/_reset.scss';
 import './assets/styles/_index.scss';
 
 import App from './App';
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
 ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 ).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/*" element={<App />} />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   </React.StrictMode>,
 );
