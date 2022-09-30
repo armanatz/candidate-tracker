eb8faaea3da58a3833650042fb394665da086636
Arman Attarzadeh <arman.atz@gmail.com>
2022-09-30 12:28:43 +0000

    Make useGetCandidates query hook more extensible

--- src/hooks/queries/candidates.ts
+++ src/hooks/queries/candidates.ts
@@ -1,36 +1,47 @@
-import { useQuery } from '@tanstack/react-query';
+import {
+  useQuery,
+  UseQueryOptions,
+} from '@tanstack/react-query';
 import useAxios from '../useAxios';
 
-export default function useGetCandidates() {
+type TQueryFnData = GetCandidatesResponse;
+type TError = Error;
+type TData = TQueryFnData;
+type TQueryKey = ['candidates'];
+
+type Options = Omit<
+  UseQueryOptions<TQueryFnData, TError, TData, TQueryKey>,
+  'queryKey' | 'queryFn'
+>;
+
+export default function useGetCandidates(opts?: Options) {
   const axios = useAxios();
 
-  const req = useQuery<GetCandidatesResponse>(
-    ['candidates'],
-    async () => {
-      const promise = new Promise<GetCandidatesResponse>(
-        (resolve, reject) => {
-          axios
-            .get('/candidates')
-            .then(res => {
-              if (
-                res.status === 200 &&
-                'error' in res.data
-              ) {
-                return reject(res.data);
-              }
+  const axiosReq = async () => {
+    const promise = new Promise<GetCandidatesResponse>(
+      (resolve, reject) => {
+        axios
+          .get('/candidates')
+          .then(res => {
+            if (res.status === 200 && 'error' in res.data) {
+              return reject(res.data);
+            }
 
-              return resolve(res.data);
-            })
-            .catch(err => {
-              return reject(err);
-            });
-        },
-      );
+            return resolve(res.data);
+          })
+          .catch(err => {
+            return reject(err);
+          });
+      },
+    );
 
-      const result = await promise;
-      return result;
-    },
-  );
+    const result = await promise;
+    return result;
+  };
 
-  return req;
+  const query = useQuery(['candidates'], axiosReq, {
+    ...opts,
+  });
+
+  return query;
 }
