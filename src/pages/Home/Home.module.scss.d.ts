b605d5e531b4292ba0475843c8644abc52ce35ea
Arman Attarzadeh <arman.atz@gmail.com>
2022-09-30 12:15:35 +0000

    Implement sorting select field on Home page

--- src/pages/Home/Home.module.scss.d.ts
+++ src/pages/Home/Home.module.scss.d.ts
@@ -1,10 +1,12 @@
 declare const classNames: {
+  readonly 'action-area': 'action-area';
   readonly title: 'title';
   readonly 'grid-container': 'grid-container';
   readonly grid: 'grid';
 };
 export default classNames;
 export type ClassNames =
+  | 'action-area'
   | 'title'
   | 'grid-container'
   | 'grid';
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,25 +1,29 @@
+import { useState } from 'react';
 import { IconUsers } from '@tabler/icons';
 
 import useGetCandidates from '../../hooks/queries/candidates';
 
-import { Card } from '../../components/DS';
+import { Card, Select, Option } from '../../components/DS';
 
-import FullPageLoader from '../../components/FullPageLoader';
 import CandidateInfo from '../../components/CandidateInfo';
 
 import styles from './Home.module.scss';
 
 export default function Home() {
+  const [sortBy, setSortBy] = useState('none');
+
   const { data: candidates, status: candidatesStatus } =
     useGetCandidates();
 
-  if (candidatesStatus === 'loading') {
-    return <FullPageLoader />;
-  }
+  const sortCandidates = (value: string) => {
+    setSortBy(value);
+  };
 
-  let candidateList = null;
+  let candidateList: JSX.Element | JSX.Element[] = (
+    <p>Loading...</p>
+  );
 
-  if (candidatesStatus !== 'error') {
+  if (candidatesStatus === 'success' && candidates.data) {
     candidateList = candidates.data?.map(candidate => (
       <Card withPadding={false} key={candidate.id}>
         <CandidateInfo
@@ -37,9 +41,37 @@ export default function Home() {
 
   return (
     <div>
-      <div className={styles.title}>
-        <IconUsers stroke={2.5} />
-        <h1>Applicants</h1>
+      <div className={styles['action-area']}>
+        <div className={styles.title}>
+          <IconUsers stroke={2.5} />
+          <h1>Applicants</h1>
+        </div>
+        <div
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            gap: '0.5em',
+          }}
+        >
+          <p>Sort By:</p>
+          <Select
+            label="Sort by"
+            placeholder="Select sorting..."
+            value={sortBy}
+            onChange={sortCandidates}
+          >
+            <Option value="none">None</Option>
+            <Option value="position_applied">
+              Position Applied
+            </Option>
+            <Option value="year_of_experience">
+              Years of Experience
+            </Option>
+            <Option value="application_date">
+              Application Date
+            </Option>
+          </Select>
+        </div>
       </div>
       <div className={styles['grid-container']}>
         <div className={styles.grid}>{candidateList}</div>
